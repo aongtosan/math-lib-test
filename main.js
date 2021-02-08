b1330f5013539146bbd762ac5f63322e6279ba75
aongtosan <aongtosan@gmail.com>
2021-02-08 16:05:28 +0700

    Refactor: Add sum array

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function sum(a,b){
     if(Array.isArray(a)){
         let total = 0
-        for(let i = 0;i<a.length;i++)
-        total+= a[i]
+        /*for(let i = 0;i<a.length;i++)
+        total+= a[i]*/
+        a.forEach((i)=>(total+=i))
         return total
     }
     return a+b
